750dc77581b0c42d54a257105408a604a188aaab
jhon <jhon_edinson11@hotmail.com>
2019-01-19 21:29:18 -0500

    adjust in the code, clean unused line codes

--- src/main/java/com/co/coapps/views/JasperView.java
+++ src/main/java/com/co/coapps/views/JasperView.java
@@ -1,19 +1,13 @@
 package com.co.coapps.views;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
@@ -21,20 +15,15 @@ import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
-import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
 import com.co.coapps.utilities.ConexionBD;
 
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporter;
-import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.util.JRLoader;
 
 @ManagedBean
 public class JasperView implements Serializable {
@@ -43,13 +32,10 @@ public class JasperView implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String name1;
-	private String name2;
+	private String diocesis;
 	private ConexionBD conexionBD;
 	private StreamedContent file;
 	
-
-
 	@PostConstruct
 	public void init() {
 		conexionBD = null;
@@ -78,25 +64,19 @@ public class JasperView implements Serializable {
 		Connection conn = conexionBD.conectar();
 
 		try {
-
-			
 			
 			Map<String, String> parametros = new HashMap<>();
-			ByteArrayOutputStream Teste = new ByteArrayOutputStream();
-			parametros.put("diocesis", name1);
+			parametros.put("diocesis", diocesis);
 
 			ExternalContext eCtx = FacesContext.getCurrentInstance().getExternalContext();
 			URL url = eCtx.getResource("/WEB-INF/resources");
+			
+			//en caso de tener espacios en la ruta que se identifique con %20
 			String ur = url.getPath().replace("%20", " ");
-			//File files = new File(ur + "/jasper/report1.jrxml");
 			
 			JasperReport jasperReport = JasperCompileManager.compileReport(ur + "/jasper/report1.jrxml");
-			//InputStream input =(InputStream) JRLoader.loadObjectFromLocation(ur + "/jasper/report1.jasper"); 
-			//(JasperReport) JRLoader.loadObjectFromLocation(files.getPath());
 			JasperPrint print = JasperFillManager.fillReport(jasperReport, parametros, conn);
 
-
-			
 			HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance()
 					.getExternalContext().getResponse();
 			ServletOutputStream servletStream = httpServletResponse.getOutputStream();
@@ -123,21 +103,12 @@ public class JasperView implements Serializable {
 	}
 
 
-
-	public String getName1() {
-		return name1;
+	public String getDiocesis() {
+		return diocesis;
 	}
 
-	public void setName1(String name1) {
-		this.name1 = name1;
-	}
-
-	public String getName2() {
-		return name2;
-	}
-
-	public void setName2(String name2) {
-		this.name2 = name2;
+	public void setDiocesis(String diocesis) {
+		this.diocesis = diocesis;
 	}
 
 	public StreamedContent getFile() {
